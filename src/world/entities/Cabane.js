a938235c91c4b3559e1ee8c6b1363fa9565e0af2
Pierre-Louis <pl74rou@gmail.com>
2026-05-13 20:11:45 +0200

    style: apply prettier formatting to Presentation, PlayerControls, SceneCharacters, Cabane

--- src/world/entities/Cabane.js
+++ src/world/entities/Cabane.js
@@ -272,9 +272,7 @@ export async function buildCabane({
 
   const root = new THREE.Group()
   root.name = 'cabane'
-  const modelBasePaths = performanceMode
-    ? ['/models/']
-    : ['/models/compressed/', '/models/']
+  const modelBasePaths = performanceMode ? ['/models/'] : ['/models/compressed/', '/models/']
   const textureBasePaths = performanceMode
     ? ['/textures/ktx2/', '/textures/compressed/', '/textures/']
     : ['/textures/ktx2/', '/textures/']
