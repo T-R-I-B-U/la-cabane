d536999148fd07bd165159fcb691fab91492727a
Pierre-Louis <pl74rou@gmail.com>
2026-05-13 10:48:04 +0200

    fix(scene): remove legacy ground plane
    Keep only the invisible fallback floor collider and lower it slightly to avoid z-fighting and collisions with the new cabane ground assets.

--- src/core/Floor.jsx
+++ src/core/Floor.jsx
@@ -1,56 +1,14 @@
-import { use, useEffect, useMemo } from 'react'
-import { DoubleSide, RepeatWrapping, SRGBColorSpace } from 'three'
 import { FLOOR_Y } from './SceneConfig'
-import { preferKtx2, loadStandaloneTexture } from '../world/cabane/textureResolver.js'
-
-const GROUND_SIZE = 400
-
-export function Floor({ mainFloorRef, hutPosition }) {
-  const grassTexture = use(
-    loadStandaloneTexture(preferKtx2('/textures/grass.png'), { colorSpace: SRGBColorSpace })
-  )
-  const tiledGrassTexture = useMemo(() => {
-    if (!grassTexture) return null
-    const texture = grassTexture.clone()
-    texture.colorSpace = SRGBColorSpace
-    texture.wrapS = RepeatWrapping
-    texture.wrapT = RepeatWrapping
-    texture.repeat.set(96, 96)
-    texture.needsUpdate = true
-    return texture
-  }, [grassTexture])
-
-  useEffect(() => {
-    return () => tiledGrassTexture?.dispose()
-  }, [tiledGrassTexture])
-
+export function Floor({ mainFloorRef }) {
   return (
-    <>
-      <mesh
-        ref={mainFloorRef}
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, FLOOR_Y, 0]}
-        userData={{ isFloor: true }}
-      >
-        <planeGeometry args={[400, 400]} />
-        <meshBasicMaterial visible={false} />
-      </mesh>
-
-      <mesh
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[hutPosition[0], -0.02, hutPosition[2]]}
-        receiveShadow
-        raycast={() => {}}
-      >
-        <planeGeometry args={[GROUND_SIZE, GROUND_SIZE]} />
-        <meshStandardMaterial
-          color="#ffffff"
-          map={tiledGrassTexture}
-          roughness={0.96}
-          metalness={0}
-          side={DoubleSide}
-        />
-      </mesh>
-    </>
+    <mesh
+      ref={mainFloorRef}
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, FLOOR_Y - 0.02, 0]}
+      userData={{ isFloor: true }}
+    >
+      <planeGeometry args={[400, 400]} />
+      <meshBasicMaterial visible={false} />
+    </mesh>
   )
 }
